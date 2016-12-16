30a628408b3449d2a18452a5799fa2e651976e88
Eric Sanders <EricLeeSanders@gmail.com>
2016-12-16 02:21:31 -0500

    Fixed mobile page scrolling bug and cleaned js
    Fixed mobile page scrolling bug and cleaned js

--- assets/portfolio.js
+++ assets/portfolio.js
@@ -1,25 +1,35 @@
+ // module pattern
+ (function ($) {
 
-/* Navbar link clicked */
-$('a[href^="#"]').on('click', function(event) {
-	 //Hide collapsable menu if need be
-    if($('.hamburger-nav').is(':visible')) {
-    	$('#hamburger-nav').attr('checked', false);
-	}
-    var target = $( $(this).attr('href') );
-    if( target.length ) {
-        event.preventDefault();
-        $('html, body').animate({
-            scrollTop: target.offset().top - $("#header").outerHeight(true)
-        }, 1000);
-    }
-
-});
-
-(function ($) {
-  $(document).ready(function(){
-	$(function () {
+	/* Navbar link clicked */
+	$('a[href^="#"]').on('click', function(event) {
+		 //Hide collapsable menu if need be
+	    if($('.hamburger-nav').is(':visible')) {
+	    	$('#hamburger-nav').attr('checked', false);
+		}
+	    var target = $( $(this).attr('href') );
+	    if( target.length ) {
+	        event.preventDefault();
+	        $('html, body').animate({
+	            scrollTop: target.offset().top - $("#header").outerHeight(true)
+	        }, 1000);
+	    }
+	
+	});
+	
+	
+	$(document).ready(function(){
+  	
+  		// Set the body to the height of the sccreen.
+  		// This is important because on mobile devices,
+  		// the URL bar takes up space when scrolling.
+  		// This causes the body / divs set at 100% to 
+  		// adjust and make the page jumpy.
+	  	var screenHeight = $(window).height();
+		$('body').css('height', screenHeight + 'px');
+	
 		$(window).scroll(function () {
-
+	
 			/* Hide/Show the Header/Home if need be */
 			if ($(this).scrollTop() > ($("#top").height())/4) {
 				$("#home-navbar").fadeOut(500, function(){});
@@ -31,28 +41,23 @@ $('a[href^="#"]').on('click', function(event) {
 				}
 				$("#header").fadeOut(500, function(){});
 			}
-
-			/* Change the selected navbar link when the section is scrolled into view */
-
-			scrollPosition	= $(this).scrollTop();
-			headerHeight = $("#header").outerHeight(true);
-			scrollOffset = $( window ).height()/4;
-		 	$('#header nav a[href^="#"]').each(function() {
 	
-				thisHref = $( $(this).attr('href') );
-				thisHrefTopPosition = $(thisHref).offset().top;
-				thisPosition = thisHrefTopPosition - headerHeight - scrollOffset;
-				
+			/* Change the selected navbar link when the section is scrolled into view */
+	
+			var scrollPosition	= $(this).scrollTop();
+			var headerHeight = $("#header").outerHeight(true);
+			var scrollOffset = $( window ).height()/4;
+			
+		 	$('#header nav a[href^="#"]').each(function() {
+				var thisHref = $( $(this).attr('href') );
+				var thisHrefTopPosition = $(thisHref).offset().top;
+				var thisPosition = thisHrefTopPosition - headerHeight - scrollOffset;
+		
 				if(scrollPosition >= thisPosition) {
-
 					$('.selectedNavItem').removeClass('selectedNavItem');
 					$(this).addClass('selectedNavItem');
-
 				}
 			});
-		});	
-		
+		});
 	});
-
-});
  }(jQuery));
